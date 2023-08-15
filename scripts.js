8aedb2b5e66727f8f2de34402f6aeb59b98322a1
TohaTanvir <87535510+TohaTanvir@users.noreply.github.com>
2023-08-15 16:31:04 +0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,22 @@
+// Smooth scrolling for navigation links
+document.addEventListener("DOMContentLoaded", function () {
+    const navLinks = document.querySelectorAll("nav ul li a");
+
+    for (const link of navLinks) {
+        link.addEventListener("click", smoothScroll);
+    }
+
+    function smoothScroll(event) {
+        event.preventDefault();
+
+        const targetId = this.getAttribute("href");
+        const targetElement = document.querySelector(targetId);
+        const targetPosition = targetElement.getBoundingClientRect().top + window.pageYOffset;
+
+        window.scrollTo({
+            top: targetPosition,
+            behavior: "smooth",
+        });
+    }
+});
 
